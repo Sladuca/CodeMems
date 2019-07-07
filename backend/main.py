501b79cf7e1971932b28a048c82a7d0fb8c8fc1a
Sebastien La Duca <sladuca777@gmail.com>
2019-07-06 22:05:16 -0400

    rename review to card

---
+++ backend/main.py
@@ -0,0 +1,27 @@
+from sanic import Sanic
+from sanic.response import json
+
+app = Sanic()
+
+@app.route("/hello")
+async def hello(request):
+    return json({"hello": "world"})
+
+@app.route("/upsert_notes")
+async def upsert_notes(request):
+    return json({"route": "unimplemented"})
+
+@app.route("/delete_notes")
+async def delete_notes(request):
+    return json({"route": "unimplemented"})
+
+@app.route("/get_cards")
+async def get_cards(request):
+    return json({"route": "unimplemented"})
+
+@app.route("/review_cards")
+async def update_cards(request):
+    return json({"route": "unimplemented"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
