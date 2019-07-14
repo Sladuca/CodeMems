db51eb54b287f517c48ce752d62e4ebdc5901a5e
Sebastien La Duca <sladuca777@gmail.com>
2019-07-14 01:50:31 -0400

    basic addNote

---
+++ frontend/src/components/AddNote.vue
@@ -0,0 +1,32 @@
+<template lang="html">
+  <div class="">
+    <cv-form>
+      <cv-text-input label="Front" v-model="front" placeholder="e.g. Capital of USA" />
+      <cv-text-input label="Back" v-model="back" placeholder="e.g. Washington D.C." />
+      <cv-button @click="clickAddNote">Add</cv-button>
+    </cv-form>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+export default {
+  name: "AddNote",
+  data() {
+    return {
+      front: "",
+      back: "",
+    }
+  },
+  methods: {
+    ...mapActions(['addNote']),
+    clickAddNote(event){
+      event.preventDefault();
+      this.addNote(this)
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
