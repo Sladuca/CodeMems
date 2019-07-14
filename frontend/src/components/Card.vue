18cdb400832b5cace66c2e74efaab79e4f886e69
Sebastien La Duca <sladuca777@gmail.com>
2019-07-14 01:51:14 -0400

    card ui handle only basic f-b card

---
+++ frontend/src/components/Card.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <div class="">
+    <div v-for="card in note.data.cards">
+      <cv-tile>
+        <p>{{card[0]}}</p>
+        <p>{{card[1]}}</p>
+      </cv-tile>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: ["note"]
+}
+</script>
+
+<style lang="css" scoped>
+</style>
