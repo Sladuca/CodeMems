37254a94c00cdc25e07b18dacfef3d161dc80900
Sebastien La Duca <sladuca777@gmail.com>
2019-07-24 16:52:08 -0400

    rough (not-thorough) is_note

--- backend/validation.py
+++ backend/validation.py
@@ -1,7 +1,19 @@
 # validate data
 
 async def is_note(note):
-    pass
+    # check well-formedness
+    if (
+        not isInstance(note, dict)
+        or "note_type" not in note
+        or "data" not in note
+        or "fields" not in note.data
+        or "cards" not in note.data
+        or not isInstance(note.data, dict)
+    ): return False
+    # check to make sure cloze field exists
+    if "cloze" not in note.data:
+        return False
+    return True
 
 async def is_card(card):
     pass
