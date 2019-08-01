c914fa16f3933cef14b7f58797f1c38fb1f5ab05
Sebastien La Duca <sladuca777@gmail.com>
2019-08-01 11:29:34 -0400

    run in debug mode for now

--- notes_api/main.py
+++ notes_api/main.py
@@ -53,4 +53,4 @@ async def update_cards(request):
     return response.json({"route": "unimplemented"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8000, Debug=True)
