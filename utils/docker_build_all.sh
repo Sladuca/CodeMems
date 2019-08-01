54c3f591fe5cf452077569b4b7557882b36eca71
Sebastien La Duca <sladuca777@gmail.com>
2019-08-01 11:28:12 -0400

    automate build all containers

---
+++ utils/docker_build_all.sh
@@ -0,0 +1,4 @@
+docker build --rm -t data_pipeline ../data_pipeline
+docker build --rm -t notes_api ../notes_api
+docker build --rm -t scheduler ../scheduler
+docker build --rm -t frontend ../frontend
