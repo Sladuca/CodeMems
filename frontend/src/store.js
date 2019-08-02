40d3bd87eea8e920b2fe5515904c44643e09a7db
Sebastien La Duca <sladuca777@gmail.com>
2019-08-02 15:36:04 -0400

    simple view state module

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,21 +3,34 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  modules: {
-    view: viewModule,
-    decks: decksModule,
-    cards: cardsModule,
-    profile: profileModule,
-    stats: statsModule
-  },
-})
-
 const viewModule = {
   state: {
     loading: false,
-    current_view: "Welcome",
-    drawer_open: false,
+    current_view: "welcome",
+  },
+  mutations: {
+    changeView (state, newView) {
+      state.current_view = newView
+    },
+    setisLoading (state, isLoading) {
+      state.loading = isLoading
+    },
+  },
+  getters: {
+    isLoading: state => {
+      return state.loading
+    },
+    currentView: state => {
+      return state.current_view
+    }
+  },
+  actions: {
+    changeView (context, newView) {
+      context.commit('changeView', newView)
+    },
+    setIsLoading (context, isLoading) {
+      context.commit('setIsLoading', isLoading)
+    },
   }
 }
 
@@ -36,3 +49,13 @@ const profileModule = {
 const statsModule = {
 
 }
+
+export default new Vuex.Store({
+  modules: {
+    view: viewModule,
+    decks: decksModule,
+    cards: cardsModule,
+    profile: profileModule,
+    stats: statsModule
+  },
+})
