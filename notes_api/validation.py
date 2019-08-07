d1d4a0beae653340971c96c3836cda99607545cb
Sladuca <sladuca777@gmail.com>
2019-08-06 23:52:16 -0400

    changed schema, move stuff around

--- notes_api/validation.py
+++ notes_api/validation.py
@@ -1,4 +1,7 @@
-# validate data
+'''
+VALIDATION
+data integrity checks for all note-related data being recieved by the server from clients.
+'''
 
 async def is_note(note):
     # check well-formedness
@@ -7,13 +10,9 @@ async def is_note(note):
         or "note_type" not in note
         or "data" not in note
         or "fields" not in note.data
-        or "cards" not in note.data
         or not isInstance(note.data, dict)
     ): return False
-    # check to make sure cloze field exists
-    if "cloze" not in note.data:
+    # check to make sure card references are not present
+    if "card" in note:
         return False
     return True
-
-async def is_card(card):
-    pass
