a6a88817fc5c31dd49ea347164428149ec99d108
Sladuca <sladuca777@gmail.com>
2019-08-16 00:58:25 -0400

    replace python with rust; initiated helloworld pub

---
+++ card_registry/src/main.rs
@@ -0,0 +1,37 @@
+use failure::Error
+use tokio
+use futures;
+use redis;
+use lapin_futures as lapin;
+use crate::lapin::{BasicProperties, Client, ConnectionProperties};
+use crate::lapin::options::{BasicPublishOptions, QueueDeclareOptions};
+use crate::lapin::types::FieldTable;
+use crate::futures::future::err;
+
+
+fn main() {
+    // get the AMQP server's address from env var
+    // default to localhost if not provided
+    let amqp_addr = std::env::var("AMQP_ADDR")
+        .unwrap_or_else(|_| "amqp://127.0.0.1:5672/%2f".into());
+    // future that pubs to "hello"
+    let amqp_client = Client::connect(&amqp_addr, ConnectionProperties::default())
+        // attempt to connect, return an Error if it has a bruh moment
+        .map_err(Error::from)
+        // when the connect future resolves without bruh moment, set up a channel
+        .and_then(|mut channel| {
+            let id = channel.id();
+            // set up the queue
+            // pass ownership of everything to inner closure
+            channel.queue_declare("hello", 
+                                  QueueDeclareOptions::default(),
+                                  FieldTable::default())
+                .and_then(move |_| {
+                    channel.basic_publish("", 
+                                          "hello", 
+                                          b"hello from the card registry".to_vec(),
+                                          BasicPublishOptions::default(),
+                                          BasicProperties::default())
+                }).map_err(Error::from)
+        });
+}
