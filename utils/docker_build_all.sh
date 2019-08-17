ccbcd90d30b4ab68fbe5e72734fa9bd482718a44
Sladuca <sladuca777@gmail.com>
2019-08-17 13:20:01 -0400

    update to new names

--- utils/docker_build_all.sh
+++ utils/docker_build_all.sh
@@ -1,4 +1,5 @@
 docker build --rm -t data_pipeline ../data_pipeline
-docker build --rm -t notes_api ../notes_api
+docker build --rm -t card_registry ../card_registry
+docker build --rm -t note_registry ../note_registry
 docker build --rm -t scheduler ../scheduler
 docker build --rm -t frontend ../frontend
