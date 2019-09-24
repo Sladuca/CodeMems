022f4f68868e67a72b107836b1ccc1d821a22c20
Sladuca <sladuca777@gmail.com>
2019-09-24 09:54:41 -0400

    add card and deck items

--- frontend/src/components/Welcome.vue
+++ frontend/src/components/Welcome.vue
@@ -1,26 +1,53 @@
 <template lang="html">
   <v-container
-    fluid
     fill-height
   >
     <v-layout
-      align-center
+      align-top
       justify-center
     >
-      <v-flex shrink>
-        <v-tooltip right>
-          <template v-slot:activator="{ on }">
-            <v-btn
-              icon
-              large
-              target="_blank"
-              @click="gotoStats()"
+      <v-flex>
+        <template>
+          <!-- Welcome message card --> 
+          <v-card
+           class="pa-4 md-4"
+          >
+            <v-card-title>
+              Welcome back, Sebastien. Are your hacker senses tingling?
+            </v-card-title>
+            <v-card-text>
+              You have 3 decks with cards to review, for a total of 99 cards
+            </v-card-text>
+            <v-card-actions>
+              <v-btn text>
+                Review Now
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+          <!-- end message card -->
+          <v-divider></v-divider>
+          <!-- Deck items --> 
+          <v-expansion-panels
+            class="pa-4 md-4"
             >
-              <v-icon large>fa-chart-line</v-icon>
-            </v-btn>
-          </template>
-          <span>Stats</span>
-        </v-tooltip>
+            <v-expansion-panel
+              v-for="(item, i) in 3"
+              :key="i"
+            >
+            <v-expansion-panel-header>
+              <v-badge
+                :left=true
+                >
+                <template v-slot:badge>33</template>
+                <span>Deck_Name</span>
+              </v-badge>
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              Review
+            </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
+        </template>
       </v-flex>
     </v-layout>
   </v-container>
