d27eef1b5af1bc0469df73df54fed608568b9414
Sladuca <sladuca777@gmail.com>
2019-10-01 21:19:37 -0400

    change drawer title

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -60,7 +60,7 @@
       clipped-left
     >
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-toolbar-title>CodeMems</v-toolbar-title>
     </v-app-bar>
 
     <v-content :currentView="currentView">
