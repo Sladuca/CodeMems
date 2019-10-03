02ecc75cd1749af70558b98f22602b659a37238c
Sladuca <sladuca777@gmail.com>
2019-10-03 09:49:23 -0400

    get k82 1.16 fix

--- utils/setup_helm_local.sh
+++ utils/setup_helm_local.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR=$(dirname $(readlink -f "$0"))
 echo "$SCRIPT_DIR"
 set -x
-helm init
+helm init --override spec.selector.matchLabels.'name'='tiller',spec.selector.matchLabels.'app'='helm' --output yaml | sed 's@apiVersion: extensions/v1beta1@apiVersion: apps/v1@' | kubectl apply -f -
 helm serve --repo-path "$SCRIPT_DIR/../charts/repo" &
 helm repo add local http://localhost:8879
 set +x
