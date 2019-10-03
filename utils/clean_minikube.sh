5903ae1505a5a6ca8bf1745a9bb9bb7589639e0c
Sladuca <sladuca777@gmail.com>
2019-10-03 09:39:34 -0400

    use kubenetes 1.16 fix

--- utils/clean_minikube.sh
+++ utils/clean_minikube.sh
@@ -1,4 +1,4 @@
 minikube delete
 minikube start
 minikube addons enable ingress
-helm init
+helm init --override spec.selector.matchLabels.'name'='tiller',spec.selector.matchLabels.'app'='helm' --output yaml | sed 's@apiVersion: extensions/v1beta1@apiVersion: apps/v1@' | kubectl apply -f -
