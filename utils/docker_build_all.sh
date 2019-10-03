0801dca00f7cd0dd42dded9963bd731bb275f7be
Sladuca <sladuca777@gmail.com>
2019-10-03 09:45:53 -0400

    cache is too worth

--- utils/docker_build_all.sh
+++ utils/docker_build_all.sh
@@ -1,5 +1,5 @@
-docker build --rm -t data_pipeline ../data_pipeline
-docker build --rm -t card_registry ../card_registry
-docker build --rm -t note_registry ../note_registry
-docker build --rm -t scheduler ../scheduler
-docker build --rm -t frontend ../frontend
+docker build -t data_pipeline ../data_pipeline
+docker build -t card_registry ../card_registry
+docker build -t note_registry ../note_registry
+docker build -t scheduler ../scheduler
+docker build -t frontend ../frontend
