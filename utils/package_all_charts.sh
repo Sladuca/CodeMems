0f879a187af520923c1013319a5ddcf61a447ee0
Sladuca <sladuca777@gmail.com>
2019-10-06 14:22:20 -0400

    stackOverflow copied spaghetti nonsense

--- utils/package_all_charts.sh
+++ utils/package_all_charts.sh
@@ -2,7 +2,7 @@ REPO="$(dirname $0)/../charts/repo"
 CHARTS="$(dirname $0)/../charts/"
 for d in CHARTS; do
   # only grab dirs
-  if [ -d "$f" ]; then
+  if [ -d "$d" ]; then
     helm package $d -d $REPO # package the chart into the repo
     helm repo index $REPO # reindex the repo with the new chart
   fi
