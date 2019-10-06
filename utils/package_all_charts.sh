1e9db484982b17f5abc946c44786a2df7005ea9c
Sladuca <sladuca777@gmail.com>
2019-10-06 14:44:06 -0400

    fix script

--- utils/package_all_charts.sh
+++ utils/package_all_charts.sh
@@ -1,9 +1,11 @@
 REPO="$(dirname $0)/../charts/repo" 
 CHARTS="$(dirname $0)/../charts/"
-for d in CHARTS; do
+for d in $CHARTS*; do
   # only grab dirs
-  if [ -d "$d" ]; then
+  if [ -d "$d" ] && ! echo "$d" | grep "repo"; then
     helm package $d -d $REPO # package the chart into the repo
     helm repo index $REPO # reindex the repo with the new chart
+  else
+    echo "skipping helm chart repo"
   fi
 done
