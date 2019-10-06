2545582e9d3ba5a5a4abd3eb36fed45f631bc182
Sladuca <sladuca777@gmail.com>
2019-10-06 12:53:57 -0400

    script for launching the app in minikube

---
+++ utils/start_in_minikube.sh
@@ -0,0 +1,3 @@
+minikube start
+minikube addons enable ingress
+helm init --override spec.selector.matchLabels.'name'='tiller',spec.selector.matchLabels.'app'='helm' --output yaml | sed 's@apiVersion: extensions/v1beta1@apiVersion: apps/v1@' | kubectl apply -f -
